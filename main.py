ed864a9d83d67011fcaff1a7a074d79ed2cb206f
STORM <stormvtube@gmail.com>
2024-09-10 09:57:44 -0400

    Sub-Class System - Theres prop a better way to do this TBH

--- main.py
+++ main.py
@@ -242,10 +242,313 @@ def char_creation():
         print("Invalid Class")
         return char_creation()
     
-    if char_class == "Barbarian"
+    if char_class == "Barbarian":
         print("")
         print("Barbarian Sub-Class's")
-        
+        print("| 1 - Path of the Berserker | 2 - Path of the Totem Warrior | 3 - Path of the Storm Herald | 4 - Path of the Zealot | 5 - Path of the Ancestral Guardian | 6 - Path of the Beast | 7 - Path of Wild Magic |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "Path of the Berserker"
+        elif char_sub_class == "2":
+            char_sub_class = "Path of the Totem Warrior"
+        elif char_sub_class == "3":
+            char_sub_class = "Path of the Storm Herald"
+        elif char_sub_class == "4":
+            char_sub_class = "Path of the Zealot"
+        elif char_sub_class == "5":
+            char_sub_class = "Path of the Ancestral Guardian"
+        elif char_sub_class == "6":
+            char_sub_class = "Path of the Beast"
+        elif char_sub_class == "7":
+            char_sub_class = "Path of Wild Magic"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Bard":
+        print("")
+        print("Bard Sub-Class's")
+        print("| 1 - College of Lore | 2 - College of Valor | 3 - College of Glamour | 4 - College of Swords | 5 - College of Whispers | 6 - College of Creation | 7 - College of Eloquence |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+         char_sub_class = "College of Lore"
+        elif char_sub_class == "2":
+            char_sub_class = "College of Valor"
+        elif char_sub_class == "3":
+            char_sub_class = "College of Glamour"
+        elif char_sub_class == "4":
+            char_sub_class = "College of Swords"
+        elif char_sub_class == "5":
+            char_sub_class = "College of Whispers"
+        elif char_sub_class == "6":
+            char_sub_class = "College of Creation"
+        elif char_sub_class == "7":
+            char_sub_class = "College of Eloquence"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Cleric":
+        print("")
+        print("Cleric Sub-Class's")
+        print("| 1 - Life Domain | 2 - Light Domain | 3 - Trickery Domain | 4 - War Domain | 5 - Tempest Domain | 6 - Knowledge Domain | 7 - Nature Domain | 8 - Grave Domain | 9 - Forge Domain | 10 - Order Domain | 11 - Peace Domain | 12 - Twilight Domain |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "Life Domain"
+        elif char_sub_class == "2":
+            char_sub_class = "Light Domain"
+        elif char_sub_class == "3":
+            char_sub_class = "Trickery Domain"
+        elif char_sub_class == "4":
+            char_sub_class = "War Domain"
+        elif char_sub_class == "5":
+            char_sub_class = "Tempest Domain"
+        elif char_sub_class == "6":
+            char_sub_class = "Knowledge Domain"
+        elif char_sub_class == "7":
+            char_sub_class = "Nature Domain"
+        elif char_sub_class == "8":
+            char_sub_class = "Grave Domain"
+        elif char_sub_class == "9":
+            char_sub_class = "Forge Domain"
+        elif char_sub_class == "10":
+            char_sub_class = "Order Domain"
+        elif char_sub_class == "11":
+            char_sub_class = "Peace Domain"
+        elif char_sub_class == "12":
+            char_sub_class = "Twilight Domain"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Druid":
+        print("")
+        print("Druid Sub-Class's")
+        print("| 1 - Circle of the Land | 2 - Circle of the Moon | 3 - Circle of the Shepherd | 4 - Circle of Spores | 5 - Circle of Dreams | 6 - Circle of Stars | 7 - Circle of Wildfire |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "Circle of the Land"
+        elif char_sub_class == "2":
+            char_sub_class = "Circle of the Moon"
+        elif char_sub_class == "3":
+            char_sub_class = "Circle of the Shepherd"
+        elif char_sub_class == "4":
+            char_sub_class = "Circle of Spores"
+        elif char_sub_class == "5":
+            char_sub_class = "Circle of Dreams"
+        elif char_sub_class == "6":
+            char_sub_class = "Circle of Stars"
+        elif char_sub_class == "7":
+            char_sub_class = "Circle of Wildfire"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Fighter":
+        print("")
+        print("Fighter Sub-Class's")
+        print("| 1 - Champion | 2 - Battle Master | 3 - Eldritch Knight | 4 - Arcane Archer | 5 - Samurai | 6 - Cavalier | 7 - Echo Knight | 8 - Psi Warrior | 9 - Rune Knight |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "Champion"
+        elif char_sub_class == "2":
+            char_sub_class = "Battle Master"
+        elif char_sub_class == "3":
+            char_sub_class = "Eldritch Knight"
+        elif char_sub_class == "4":
+            char_sub_class = "Arcane Archer"
+        elif char_sub_class == "5":
+            char_sub_class = "Samurai"
+        elif char_sub_class == "6":
+            char_sub_class = "Cavalier"
+        elif char_sub_class == "7":
+            char_sub_class = "Echo Knight"
+        elif char_sub_class == "8":
+            char_sub_class = "Psi Warrior"
+        elif char_sub_class == "9":
+            char_sub_class = "Rune Knight"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Monk":
+        print("")
+        print("Monk Sub-Class's")
+        print("| 1 - Way of the Open Hand | 2 - Way of Shadow | 3 - Way of the Four Elements | 4 - Way of the Kensei | 5 - Way of the Drunken Master | 6 - Way of the Sun Soul | 7 - Way of Mercy | 8 - Way of the Astral Self |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "Way of the Open Hand"
+        elif char_sub_class == "2":
+            char_sub_class = "Way of Shadow"
+        elif char_sub_class == "3":
+            char_sub_class = "Way of the Four Elements"
+        elif char_sub_class == "4":
+            char_sub_class = "Way of the Kensei"
+        elif char_sub_class == "5":
+            char_sub_class = "Way of the Drunken Master"
+        elif char_sub_class == "6":
+            char_sub_class = "Way of the Sun Soul"
+        elif char_sub_class == "7":
+            char_sub_class = "Way of Mercy"
+        elif char_sub_class == "8":
+            char_sub_class = "Way of the Astral Self"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Paladin":
+        print("")
+        print("Paladin Sub-Class's")
+        print("| 1 - Oath of Devotion | 2 - Oath of Vengeance | 3 - Oath of the Ancients | 4 - Oath of Conquest | 5 - Oath of Redemption | 6 - Oath of Glory | 7 - Oath of the Crown | 8 - Oath of the Watchers |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "Oath of Devotion"
+        elif char_sub_class == "2":
+            char_sub_class = "Oath of Vengeance"
+        elif char_sub_class == "3":
+            char_sub_class = "Oath of the Ancients"
+        elif char_sub_class == "4":
+            char_sub_class = "Oath of Conquest"
+        elif char_sub_class == "5":
+            char_sub_class = "Oath of Redemption"
+        elif char_sub_class == "6":
+            char_sub_class = "Oath of Glory"
+        elif char_sub_class == "7":
+            char_sub_class = "Oath of the Crown"
+        elif char_sub_class == "8":
+            char_sub_class = "Oath of the Watchers"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Ranger":
+        print("")
+        print("Ranger Sub-Class's")
+        print("| 1 - Hunter | 2 - Beast Master | 3 - Gloom Stalker | 4 - Horizon Walker | 5 - Monster Slayer | 6 - Fey Wanderer | 7 - Swarmkeeper | 8 - Drakewarden |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "Hunter"
+        elif char_sub_class == "2":
+            char_sub_class = "Beast Master"
+        elif char_sub_class == "3":
+            char_sub_class = "Gloom Stalker"
+        elif char_sub_class == "4":
+            char_sub_class = "Horizon Walker"
+        elif char_sub_class == "5":
+            char_sub_class = "Monster Slayer"
+        elif char_sub_class == "6":
+            char_sub_class = "Fey Wanderer"
+        elif char_sub_class == "7":
+            char_sub_class = "Swarmkeeper"
+        elif char_sub_class == "8":
+            char_sub_class = "Drakewarden"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Rogue":
+        print("")
+        print("Rogue Sub-Class's")
+        print(" | 1 - Thief | 2 - Assassin | 3 - Arcane Trickster | 4 - Mastermind | 5 - Swashbuckler | 6 - Inquisitive | 7 - Scout | 8 - Phantom | 9 - Soulknife |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "Thief"
+        elif char_sub_class == "2":
+            char_sub_class = "Assassin"
+        elif char_sub_class == "3":
+            char_sub_class = "Arcane Trickster"
+        elif char_sub_class == "4":
+            char_sub_class = "Mastermind"
+        elif char_sub_class == "5":
+            char_sub_class = "Swashbuckler"
+        elif char_sub_class == "6":
+            char_sub_class = "Inquisitive"
+        elif char_sub_class == "7":
+            char_sub_class = "Scout"
+        elif char_sub_class == "8":
+            char_sub_class = "Phantom"
+        elif char_sub_class == "9":
+            char_sub_class = "Soulknife"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Warlock":
+        print("")
+        print("Warlock Sub-Class's")
+        print("| 1 - The Fiend | 2 - The Great Old One | 3 - The Archfey | 4 - The Celestial | 5 - The Hexblade | 6 - The Fathomless | 7 - The Genie | 8 - The Undead |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "The Fiend"
+        elif char_sub_class == "2":
+            char_sub_class = "The Great Old One"
+        elif char_sub_class == "3":
+            char_sub_class = "The Archfey"
+        elif char_sub_class == "4":
+            char_sub_class = "The Celestial"
+        elif char_sub_class == "5":
+            char_sub_class = "The Hexblade"
+        elif char_sub_class == "6":
+            char_sub_class = "The Fathomless"
+        elif char_sub_class == "7":
+            char_sub_class = "The Genie"
+        elif char_sub_class == "8":
+            char_sub_class = "The Undead"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Wizard":
+        print("")
+        print("Wizard Sub-Class's")
+        print("| 1 - School of Evocation | 2 - School of Conjuration | 3 - School of Divination | 4 - School of Enchantment | 5 - School of Illusion | 6 - School of Necromancy | 7 - School of Transmutation | 8 - School of Abjuration | 9 - Bladesinging | 10 - War Magic | 11 - Chronurgy Magic | 12 - Graviturgy Magic | 13 - Scribes (Order of Scribes) |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "School of Evocation"
+        elif char_sub_class == "2":
+            char_sub_class = "School of Conjuration"
+        elif char_sub_class == "3":
+            char_sub_class = "School of Divination"
+        elif char_sub_class == "4":
+            char_sub_class = "School of Enchantment"
+        elif char_sub_class == "5":
+            char_sub_class = "School of Illusion"
+        elif char_sub_class == "6":
+            char_sub_class = "School of Necromancy"
+        elif char_sub_class == "7":
+            char_sub_class = "School of Transmutation"
+        elif char_sub_class == "8":
+            char_sub_class = "School of Abjuration"
+        elif char_sub_class == "9":
+            char_sub_class = "Bladesinging"
+        elif char_sub_class == "10":
+            char_sub_class = "War Magic"
+        elif char_sub_class == "11":
+            char_sub_class = "Chronurgy Magic"
+        elif char_sub_class == "12":
+            char_sub_class = "Graviturgy Magic"
+        elif char_sub_class == "13":
+            char_sub_class = "Order of Scribes"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
+
+    if char_class == "Artificer":
+        print("")
+        print("Artificer Sub-Class's")
+        print("| 1 - Alchemist | 2 - Armorer | 3 - Artillerist | 4 - Battle Smith |")
+        char_sub_class = input("Character Sub-Class: ")
+        if char_sub_class == "1":
+            char_sub_class = "Alchemist"
+        elif char_sub_class == "2":
+            char_sub_class = "Armorer"
+        elif char_sub_class == "3":
+            char_sub_class = "Artillerist"
+        elif char_sub_class == "4":
+            char_sub_class = "Battle Smith"
+        else:
+            print("Invalid Sub-Class")
+            return char_creation()
 
     return char_name, char_Gender, char_age, char_race, char_sub_race, char_class, char_sub_class
 
@@ -258,6 +561,7 @@ print(f"Character Age: {char_age}")
 print(f"Character Race: {char_race}")
 print(f"Character Sub-Race: {char_sub_race}")
 print(f"Character Class: {char_class}")
+print(f"Character Sub-Class: {char_sub_class}")
 print("----------------------------")
 print("")
 def confirm():
